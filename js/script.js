7c2c058cd69f9d30d2166bc50f31a5019c592241
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-09-11 21:24:43 +0200

    Final correction

--- js/script.js
+++ js/script.js
@@ -28,13 +28,15 @@
     hash: true,
     contain: true,
   });
-  //kod do paska postępu
-  var progressBar = document.querySelector('.progress-bar')
 
-  flkty.on( 'scroll', function( progress ) {
-    progress = Math.max( 0, Math.min( 1, progress ) );
+  //kod do paska postępu
+  var progressBar = document.querySelector('.progress-bar');
+
+  flkty.on( 'scroll', function(progress) {
+    progress = Math.max( 0, Math.min( 1, progress) );
     progressBar.style.width = progress * 100 + '%';
   });
+
   // kod do guzika restart cofającego karuzelę do pierwszego slajdu
   var button = document.querySelector('.button');
   var buttons = button.querySelector('.button-restart');
@@ -45,8 +47,10 @@
     if ( !matchesSelector( event.target, '.button-restart' ) ) {
       return;
     }
+
     var index = buttons.indexOf( event.target );
     flkty.selectCell( 0 );
+
   });
 
   // Dodajemy mapę do projektu poprzez  zdefiniowanie funkcji initMap w zakresie globalnym (czyli jako właściwość obiektu window).
@@ -54,29 +58,36 @@
 
     var infos = document.getElementById('infos');
 
-    // Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne pierwszego slajdu.
+    // Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne drugiego slajdu.
     var gordes = {lat: 43.9110641, lng: 5.2002043};
 
-    // Wyśrodkowanie mapy na współrzędne z pierwszego slajdu. 
+    // Wyśrodkowanie mapy na współrzędne z drugiego slajdu. 
     var map = new google.maps.Map(document.getElementById('map'),
-    { zoom: 10,
+    { 
+      zoom: 10,
       center: gordes
-    });
+    }
+    
+  );
 
     // Pętla dodająca po jednym markerze do każdego slajdu
     for (i = 0; i < slideShow.length; i++) {
-      var marker = new google.maps.Marker({
+      var marker = new google.maps.Marker(
+      {
         position: slideShow[i].coords,
         map: map
-      })
+      }
+    )
+
       marker.addListener('click', function(event) {
         //infos.innerHTML = 'You clicked ' + slideShow[i].description;
         flkty.select(i);
+
       });
 
     }
 
-    flkty.on( 'change', function( i ) {
+    flkty.on( 'change', function(i) {
 
       map.panTo(slideShow[i].coords);
 			map.setZoom(12);
